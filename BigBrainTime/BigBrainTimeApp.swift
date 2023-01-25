5f5a70205f8cba0f9921b4474d9abde544a47d05
Miroslav Bořek <boremiro@seznam.cz>
2023-01-25 08:37:16 +0100

    Initial Commit

---
+++ BigBrainTime/BigBrainTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBrainTimeApp.swift
+//  BigBrainTime
+//
+//  Created by Miroslav Bořek on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BigBrainTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
